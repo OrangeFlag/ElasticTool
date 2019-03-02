1f62b61a1e84dbbe525ccfd3279e297da349585a
i.selivanov <i.selivanov@localhost.localdomain>
2019-03-02 16:08:02 +0300

    Make cli, make example from elastic library

--- main.go
+++ main.go
@@ -1,23 +1,102 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
+	"github.com/olivere/elastic"
 	"github.com/urfave/cli"
 )
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "greet"
-	app.Usage = "fight the loneliness!"
-	app.Action = func(c *cli.Context) error {
-		fmt.Println("Hello friend!")
-		return nil
+	app.Name = "ElasticTool"
+	app.Usage = "a command line interface for managing a elasticsearch."
+
+	var (
+		host   string
+		port   string
+		client *elastic.Client
+		err    error
+	)
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "host, H",
+			Value:       "127.0.0.1",
+			Usage:       "Hostname or IP address.",
+			Destination: &host,
+		},
+		cli.StringFlag{
+			Name:        "port, p",
+			Value:       "9200",
+			Usage:       "Port number.",
+			Destination: &port,
+		},
 	}
 
-	err := app.Run(os.Args)
+	app.Commands = []cli.Command{
+		{
+			Name:  "cat",
+			Usage: "cat API",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+			Subcommands: []cli.Command{
+				{
+					Name:  "indices",
+					Usage: "cat indices",
+					Action: func(c *cli.Context) error {
+						return nil //TODO
+					},
+					Subcommands: []cli.Command{
+						{
+							Name:  "count",
+							Usage: "count of indices",
+							Action: func(c *cli.Context) error {
+								return nil //TODO
+							},
+						},
+					},
+					Flags: []cli.Flag{
+						cli.BoolFlag{
+							Name:  "all, a",
+							Usage: "include system indexes",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	app.Before = func(c *cli.Context) error {
+		//fmt.Println(host + ":" + port)
+
+		client, err = elastic.NewClient(elastic.SetURL("http://" + host + ":" + port))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//indeces, err := client.CatIndices().Do(context.Background())
+		//fmt.Println(indeces)
+		//
+		//health, err := client.ClusterHealth().Do(context.Background())
+		//
+		//if err != nil || health == nil {
+		//	return err
+		//}
+		//
+		//fmt.Println(health)
+
+		return nil
+
+	}
+
+	app.Action = func(c *cli.Context) error {
+		return nil //TODO
+	}
+
+	err = app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
